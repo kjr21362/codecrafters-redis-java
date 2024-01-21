43c710cc1d69c545756b8aeaaf6e97d864f5fdaa
kjr21362 <kjr21362@gmail.com>
2024-01-21 13:06:41 -0800

    add key expiry

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -5,25 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
 
 @AllArgsConstructor
 public class RequestHandler implements Runnable {
 
     private Socket clientSocket;
+
     @Override
     public void run() {
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true)) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true)) {
             String clientCommand;
             HashMap<String, String> store = new HashMap<>();
+            HashMap<String, LocalDateTime> expiry = new HashMap<>();
+            int arrayLen = 0;
 
             while ((clientCommand = bufferedReader.readLine()) != null) {
                 System.out.println("clientCommand: " + clientCommand);
                 if (clientCommand.startsWith("*")) {
-                    int arrayLen = Integer.valueOf(clientCommand.substring(1));
+                    arrayLen = Integer.valueOf(clientCommand.substring(1));
                 } else if (Character.isLetter(clientCommand.charAt(0))) {
                     switch (clientCommand) {
                         case "ping":
@@ -41,16 +43,34 @@ public class RequestHandler implements Runnable {
                             String key = bufferedReader.readLine();
                             bufferedReader.readLine();
                             String value = bufferedReader.readLine();
+                            int duration_ms = 0;
+                            if (arrayLen == 5) {
+                                bufferedReader.readLine();
+                                bufferedReader.readLine(); // px
+                                bufferedReader.readLine();
+                                duration_ms = Integer.valueOf(bufferedReader.readLine());
+                            }
                             store.put(key, value);
+                            if (duration_ms > 0) {
+                                expiry.put(key, LocalDateTime.now().plusNanos(duration_ms * 1000000));
+                            }
                             printWriter.print("$" + "OK".length() + "\r\n" + "OK" + "\r\n");
                             printWriter.flush();
                             break;
                         case "get":
                             bufferedReader.readLine();
                             String get_key = bufferedReader.readLine();
-                            String get_value = store.get(get_key);
-                            printWriter.print("$" + get_value.length() + "\r\n" + get_value + "\r\n");
-                            printWriter.flush();
+                            if (!expiry.containsKey(get_key) || expiry.get(get_key).isAfter(LocalDateTime.now())) {
+                                String get_value = store.get(get_key);
+                                printWriter.print("$" + get_value.length() + "\r\n" + get_value + "\r\n");
+                                printWriter.flush();
+                            } else {
+                                store.remove(get_key);
+                                expiry.remove(get_key);
+                                printWriter.print("$-1\r\n");
+                                printWriter.flush();
+
+                            }
                             break;
                     }
                 }
